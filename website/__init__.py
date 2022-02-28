74cb45c015325d33a3878e8c255398ef0b41c599
Spotarachy <wilson.f.mario@gmail.com>
2022-02-28 18:45:01 +0000

    place the database models

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flash_sqlalchemy import SQLAlchemy
-
+from os import path
 bd = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -14,7 +14,16 @@ db.init_app(APP)
     from .views import views
     from .auth import auth
 
+    import .Models 
+    from .models import *
+
     APP.register_blueprint(views, url_prefix='/')
     APP.register_blueprint(auth, url_prefix='/')
+    
+    create_database(APP)
 
     return  APP
+def create_database(APP):
+    if not path.exists('website/ + DB_NAME'):
+        db.create_all(APP=APP)
+        print('Created Database!')
