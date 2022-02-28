715fa8ead69fb37bab89aa88698ffaf6a95af5d4
Spotarachy <wilson.f.mario@gmail.com>
2022-02-28 15:36:07 +0000

    setup flask sqlalchemy database

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flash_sqlalchemy import SQLAlchemy
+
+bd = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_APP():
-    (Flask(__name__)).config['SECRET_KEY'] = 'Mario Wilson'
+    APP = Flask(__name__)
+    APP.config['SECRET_KEY'] = 'Mario Wilson'
+    APP.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NANE}'
+
+db.init_app(APP)
     
     from .views import views
     from .auth import auth
