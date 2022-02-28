8a9e8512811858ba789b78d14c7f59707781ccd2
Spotarachy <wilson.f.mario@gmail.com>
2022-02-28 14:07:42 +0000

    General commit save all

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template
+import os
+from flask import Blueprint, render_template, request, flask, redirect, request, url_for, session, flash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
     return render_template("login.html", boolean=True)
 
 
@@ -11,6 +16,35 @@ def login():
 def logout():
     return "<p>Logout</p>" 
 
-@auth.route('/sign-up')
-def sing_up():
-    return render_template("sign_up.html")
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = reques.form.get('firstName')
+        password1 = reques.form.get('password1')
+        password2 = reques.form.get('password2')
+
+    if len(email) < 4:
+        pass
+    elif len(firstName) < 2:
+        pass
+    elif password1 != password2:
+        pass
+    elif len(password1) < 6:
+        pass
+    else:
+        #add user to database
+
+    if len(email) < 4:
+        flask('Email cant less then 3 chararcters.'category='Oops')
+    elif len(firstName) < 2:
+        flask('firstName cant less then 1 chararcters.'category='Oops Sorry')
+    elif password1 != password2:
+        flask('password don\'t match.'category='Oops')
+    elif len(password1) < 6:
+        flask('Email cant less then 6 chararcters.'category='Oops')
+    else:
+        flash('Accoount created!', category='success')
+        pass
+        #add user to database
+return render_template("sign_up.html")
--- website/static/index.js
+++ website/static/index.js
@@ -7,3 +7,20 @@ let box6 = document.getElementById("box1");
 let box7 = document.getElementById("box1");
 let box8 = document.getElementById("box1");
 let box9 = document.getElementById("box1");
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+  Toast.fire({
+    icon: 'success',
+    title: 'Signed in successfully'
+  })
