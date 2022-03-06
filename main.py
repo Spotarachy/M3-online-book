fb83da62a0b80d568f921b3615996114fd28640e
Spotarachy <wilson.f.mario@gmail.com>
2022-03-06 23:29:00 +0000

    setting up all route  folder

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 APP = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "Hello,world"
+    return render_template("base.html")
+
+
+@app.route("/addcomics")
+def addcomics():
+    return render_template("addcomics.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template("sing_up.html")
+
+
 
 if __name__ == "__main__":
     app.run(
