37723b195ac44a0779490fb8b810f3afd869bdf5
Spotarachy <wilson.f.mario@gmail.com>
2022-03-23 11:50:16 +0000

    backend sign_up from

--- app.py
+++ app.py
@@ -57,11 +57,69 @@ def index():
 def login():
     return render_template("login.html")
 
-
-@app.route("/sign_up")
+##NEW
+@app.route("/sign_up", methods=["GET, POST"])
 def sign_up():
+    """
+    User is going to register their Account if the statements were correctly are use the word see if the current email address is already in the system or not
+    """
+    if request.method == "POST ":
+        user = mongo.db.users
+        email_address = request.form.get("email_address")
+        password = request.form.get("password")
+        confirmed_password = request.form.get("confirm_password")
+        active_user = user.find_one({"email_Address": username.lower()})
+
+    if active_user:
+        flask("This User Email Address already exist", category="error")
+        return redirect(url_for("sign_up"))
+
+    elif len(email_Address) < 3:
+        flash("Email Address must be more then 3 Characters", category="error")
+        return redirect(url_for("sign_up"))
+
+    elif len(password) < 6:
+        flash("Password must be more then  Characters", category="error")
+        return redirect(url_for("sign_up"))
+
+    elif password !=confirmed_password:
+        flash("Password Correct", category="error")
+        return redirect(url_for("sign_up"))
+
+    sign_up = {
+        "email_address": request.form.get("email_address","username").lower(),
+        "password": generate_password_hash(password, method="sha256")
+    }
+    
+    user.insert_one(sign_up, register)
+
+    session["email_address"] = request.form.get("email_address").lower(),
+    flash("Sing UP Successfull", category = "success")
+    return redirect(url_for("profile", sign_up = session["email_address"]))
+           
     return render_template("sign_up.html")
 
+@app.route("r")
+
+  
+
+
+
+
+
+
+
+
+
+
+
+## NEW
+##comic_id if needed    \/
+@app.route("/delete/comic_id")
+def delete (comic_id):
+    mongo.db.comic.remove({"_id": ObjectId(comic_id)})
+    flask("Your Comic has been Deleted")
+    return redirect(url_for("index.html"))
 
 @app.errorhandler(500)
 def server_error(e):
@@ -76,7 +134,7 @@ def server_error(e):
         If the page doesn't load, the user will see this message.
             """
     return render_template("404.html")
-
+##
 
 if __name__ == "__main__":
     app.run(
