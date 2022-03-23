e0a4ac113c793645e7359adf9d43be65f03c269c
Spotarachy <wilson.f.mario@gmail.com>
2022-03-23 17:39:13 +0000

    backend works

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-import os import path 
+import os
 import pymongo
 from flask import (
-     Flask, render_template, url_for, request, redirect)
-from .env import MONGO_URI 
+     Flask, render_template, url_for, request, redirect, flash, session)
+##from .env import MONGO_URI 
 from flask_login import UserMixin
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from os import path
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__, template_folder="website/template")
+
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
 MONGO = PyMongo(app)
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
 os.environ['MONGO_URI'] = 'mongodb+srv://M3:Spotarachy21Ronin@xo21.hjgyv.mongodb.net/my1DB?retryWrites=true&w=majority'
 
 #NEW
@@ -32,37 +35,28 @@ def mongo_connect(url):
 
 #DB_NAME = "database.db"
 
-@app.route("/home")
-def home():
-    return render_template("home.html")
-
-
 @app.route("/")
 def index():
     """
-        Will take a user to the index page.
-            """
-
+    Will take a user to the index page.
+    """
     ArtBook = MONGO.db.ArtBook.insert_one({
-    'first': 'Junji',
-    'last': 'Ito',
-    'comic_name': 'Uzumaki',
-    'Language': 'Japanese',
-    'page_numbers': '1043',
-    'date_sub': '31/01/2020'
+   
 })
     print(ArtBook)
     return render_template("base.html",)
 
 ##New
-@app.route("/login", methods=["GET, POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     """
     Allow users to log into their existing account, the function is to see if the username or email address already exists in the database and add accordingly to tell users this person already exists.
     """
-    if request.method == "POST ":
+    print(request.method)
+
+    if request.method == "POST":
         user = mongo.db.users
-        current_user = user.find_one ({"email_address": request.form.get().lower()})
+        current_user = user.find_one({"email_address": request.form.get().lower()})
         if check_password_hash (current_user["password"],request.form.get("password")):
             session["email_address"] = request.form.get("email_address","username").lower()
             return(url_for("profile", sign_up=session["email_address"]))
@@ -74,48 +68,53 @@ def login():
     return render_template("login.html")
 
 ##NEW
-@app.route("/sign_up", methods=["GET, POST"])
+@app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
     """
     User is going to register their Account if the statements were correctly are use the word see if the current email address is already in the system or not
     """
-    if request.method == "POST ":
-        user = mongo.db.users
-        email_address = request.form.get("email_address")
-        password = request.form.get("password")
-        confirmed_password = request.form.get("confirm_password")
-        active_user = user.find_one({"email_Address": username.lower()})
+    print(request.method)
 
-    if active_user:
-        flask("This User Email Address already exist", category="error")
-        return redirect(url_for("sign_up"))
+    if request.method == "POST":
+        user = MONGO.db.user
+        email_address = request.form.get("email")
+        password = request.form.get("password1")
+        confirmed_password = request.form.get("password2")
+        active_user = user.find_one({"email_Address":email_address})
+        print(email_address, password)
 
-    elif len(email_Address) < 3:
-        flash("Email Address must be more then 3 Characters", category="error")
-        return redirect(url_for("sign_up"))
+        if active_user:
+            flash("This User Email Address already exist", category="error")
+            print('taking')
+            return redirect(url_for("sign_up"))
+        
 
-    elif len(password) < 6:
-        flash("Password must be more then  Characters", category="error")
-        return redirect(url_for("sign_up"))
+        elif len(email_address) < 3:
+            flash("Email Address must be more then 3 Characters", category="error")
+            print('true')
+            return redirect(url_for("sign_up"))
 
-    elif password !=confirmed_password:
-        flash("Password Correct", category="error")
-        return redirect(url_for("sign_up"))
+        elif len(password) < 6:
+            flash("Password must be more then  Characters", category="error")
+            return redirect(url_for("sign_up"))
 
-    sign_up = {
-        "email_address": request.form.get("email_address","username").lower(),
-        "password": generate_password_hash(password, method="sha256")
-    }
-    
-    user.insert_one(sign_up, register)
+        # elif password !=confirmed_password:
+        #     flash("Password Correct", category="error")
+        #     return redirect(url_for("sign_up"))
 
-    session["email_address"] = request.form.get("email_address").lower(),
-    flash("Sing UP Successfull", category = "success")
-    return redirect(url_for("profile", sign_up=session["email_address"]))
-           
+        sign_up = {
+            "email_address": request.form.get("email").lower(),
+            "password": generate_password_hash(password, method="sha256")
+        }
+        
+        user.insert_one(sign_up)
+
+        session["email_address"] = request.form.get("email").lower(),
+        flash("Sing UP Successfull", category = "success")
+        return redirect(url_for("index", sign_up=session["email_address"]))
+        
     return render_template("sign_up.html")
 
-@app.route("r")
 
 ## NEW
 ##comic_id if needed    \/
@@ -142,6 +141,6 @@ def server_error(e):
 
 if __name__ == "__main__":
     app.run(
-        host = os.environ.get("IP", "0.0.0.0"),
-        port = int(os.environ.get("PORT", "5000")),
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
