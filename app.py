326c2576e9a83b85a36e30e2a68e4bbc88da947f
Spotarachy <wilson.f.mario@gmail.com>
2022-03-23 09:59:30 +0000

    error page set up

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-from os import path 
-if path.exists("env.py"):
-    import env
-from flask import Flask, render_template
+from os
+import path 
+import pymongo
+from flask import (
+     Flask, render_template, url_for, request, redirect)
 from .env import MONGO_URI 
 from flask_login import UserMixin
-from flask import url_for 
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
-# from .app import User
-# from . import db.   
+if os.path.exists("env.py"):
+    import env
 
-
-app = Flask(__name__, template_folder='website/template')
-
-
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-
-
-DB_NAME = "database.db"
-
-
+app = Flask(__name__, template_folder="website/template")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 MONGO = PyMongo(app)
 
+#NEW
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "my1DB"
+COLLECTION = "ArtBook"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+#DB_NAME = "database.db"
 
 @app.route("/home")
 def home():
@@ -31,6 +37,10 @@ def home():
 
 @app.route("/")
 def index():
+    """
+        Will take a user to the index page.
+            """
+
     ArtBook = MONGO.db.ArtBook.insert_one({
     'first': 'Junji',
     'last': 'Ito',
@@ -40,7 +50,7 @@ def index():
     'date_sub': '31/01/2020'
 })
     print(ArtBook)
-    return render_template("base.html")
+    return render_template("base.html",)
 
 
 @app.route("/login")
@@ -53,8 +63,23 @@ def sign_up():
     return render_template("sign_up.html")
 
 
+@app.errorhandler(500)
+def server_error(e):
+    """
+        If the page doesn't load, the user will see this message.
+            """
+    return render_template("500.html")
+
+@app.errorhandler(404)
+def server_error(e):
+    """
+        If the page doesn't load, the user will see this message.
+            """
+    return render_template("404.html")
+
+
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        host = os.environ.get("IP", "0.0.0.0"),
+        port = int(os.environ.get("PORT", "5000")),
         debug=True)
--- mongo.py
+++ mongo.py
@@ -8,27 +8,31 @@ MONGO_URI = os.environ.get("MONGO_URI")
 DATABASE = "my1DB"
 COLLECTION = "ArtBook"
 
-
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
         print("Mongo is connected")
         return conn
     except pymongo.errors.ConnectionFailure as e:
-        print("Could not connected: %s") % e
-        
+        print("Could not connect to MongoDB: %s") % e
 
 conn = mongo_connect(MONGO_URI)
 
-
 coll = conn[DATABASE][COLLECTION]
+
+new_doc = {"first": "max", "last": "mass", "Comic": "elastic_man", "Sub_date": "12-03-2021"}
+
+coll.insert(new_doc)
 
 
 documents = coll.find()
 
 
 for doc in documents:
-    print(ok)
+    print(doc)
 
 
 """ Insert a single document """
+
+#new_docs =
+
